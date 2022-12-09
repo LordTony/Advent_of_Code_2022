edb4a319f9fe0d7fd6aa83c055e3ba1c19e8822d
Tony Santi <antony.santi@hcss.com>
2022-12-09 09:34:43 -0600

    getting part 2 of day 8

--- d8.py
+++ d8.py
@@ -1,7 +1,17 @@
-g, s = [x.strip() for x in open("d8.txt","r").readlines()], 0
+g, s, i = [x.strip() for x in open("d8.txt","r").readlines()], 0, 0
+
 m = lambda l: max(l) if len(l) else "!"
+def view(start, l):
+    sum = 0
+    for x in l:
+        sum += 1
+        if x >= start:
+            break
+    return sum
+    
 for x,l in enumerate(g):
     for y,v in enumerate(l):
-        s += v > min(m(l[0:y]), m(l[y+1::]), m([t[y] for t in g[0:x]]), m([t[y] for t in g[x+1::]]))
-        
-print(s)
+        left, right, up, down = list(reversed(l[0:y])), l[y+1::], list(reversed([t[y] for t in g[0:x]])), [t[y] for t in g[x+1::]]
+        s += v > min(m(left), m(right), m(up), m(down))
+        i = max(i, view(v,left) * view(v,right) * view(v,up) * view(v,down))
+print(s, i)
