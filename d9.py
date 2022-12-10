2bdb51234d91ec0ea166ce0784b5a36a529e367d
Tony Santi <antony.santi@hcss.com>
2022-12-09 20:02:30 -0600

    Resisting the urge to shirnk the code

--- d9.py
+++ d9.py
@@ -1,12 +1,13 @@
-steps = [x.split(" ") for x in open("d9.txt","r").read().splitlines()]
+from numpy import sign
+
 head, tail, c  = [0,0], [0,0], set([0,0])
 
-def closeEnough(h,t): 
+def closeEnough(h,t):
     return abs(h[0] - t[0]) < 2 and abs(h[1] - t[1]) < 2
 
 def getCloser(h, t):
-    t[0] += (t[0] < h[0]) - (t[0] > h[0])
-    t[1] += (t[1] < h[1]) - (t[1] > h[1])
+    t[0] += sign(h[0] - t[0])
+    t[1] += sign(h[1] - t[1])
     
 def help(index, direction, distance):
     head[index] += direction * distance
@@ -19,7 +20,7 @@ R = lambda d: help(0,  1, d)
 U = lambda d: help(1,  1, d)
 D = lambda d: help(1, -1, d)      
 
-for s in steps:
+for s in [x.split(" ") for x in open("d9.txt","r").read().splitlines()]: 
     eval(f"{s[0]}({s[1]})")
     
 print(len(c))
